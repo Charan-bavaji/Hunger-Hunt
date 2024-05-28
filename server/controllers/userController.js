4d5df1ceac365d441c51884c7c8f39757508599a
manu <bmmanu555@gmail.com>
2024-05-28 20:21:31 +0530

    Modified userController
    updated Register Controller using zod

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,25 +1,35 @@
 const express = require('express');
-const User = require('../models/authSchema');
+const {User} = require('../models/authSchema');
+const { RegisterUser } = require('../config/types');
 
 
-// Register
+
+// Register Controller
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
+    const details = req.body;
+    const parsedDetails = RegisterUser.safeParse(details);
+    if(!parsedDetails.success) {
+        const errors = parsedDetails.error.errors.map(err => ({ field: err.path[0], message: err.message }));
+        res.status(400).json({
+            errors: errors
+        })
+        return;
+    }
 
     try {
-        let user = await User.findOne({ email });
+        let user = await User.findOne({ email: details.email });
         if (user) {
-            return res.status(400).json({ mag: "User already exist" });
+            return res.status(400).json({ msg: "User already exist" });
         }
 
         // Creating new User
         user = new User({
-            name,
-            email,
-            password,
+            name: details.name,
+            email: details.email,
+            password: details.password,
         });
         await user.save();
-        res.send('Registered Successfully');
+        res.status(200).json({msg: "Registered Successfully"});
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ msg: "Server Error" });
@@ -29,3 +39,6 @@ const registerUser = async (req, res) => {
 module.exports = {
     registerUser
 }
+
+
+
